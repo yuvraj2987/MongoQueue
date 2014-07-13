3b1afa204f8ee869c3ec26bee3c472a6e1b20df6
Amit Kulkarni <yuvraj2987@gmail.com>
2014-07-13 04:34:51 +0000

    Added drop method to queue

--- Mongo/queue.py
+++ Mongo/queue.py
@@ -4,14 +4,23 @@
 class MongoQueue():
 
     def __init__(self, mongo_client, queue_name):
+        self.client = mongo_client
         self.db = mongo_client.messages
         self.coll = self.db[queue_name]
+    
 
     def add(self, item):
+        #self.client.write_concern={"w":1}
         self.coll.insert({"item":item})
 
     def get(self):
         
+        #self.client.write_concern = {"w":1}
         doc = self.coll.find_and_modify(query={}, sort={"_id":1}, remove=True)
         return doc["item"]
 
+    def drop(self):
+        """
+        return True if this collection is dropped successfully
+        """
+        return self.db.drop_collection(self.coll)
