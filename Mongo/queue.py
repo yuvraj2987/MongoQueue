9392f3b1ec56071af7baef0add09f3d62e62eb58
Amit Kulkarni <yuvraj2987@gmail.com>
2014-07-13 04:20:55 +0000

    Implemented queue interface

--- Mongo/queue.py
+++ Mongo/queue.py
@@ -1,18 +1,17 @@
 
 
-class Queue():
 
-    def __init__(self, mongo_client, collection_name):
+class MongoQueue():
+
+    def __init__(self, mongo_client, queue_name):
         self.db = mongo_client.messages
-        self.collection = self.db[collection_name]
-        #keys = {"item":1, "_id":1}
-        #self.collection.ensure_index(keys)
+        self.coll = self.db[queue_name]
+
+    def add(self, item):
+        self.coll.insert({"item":item})
 
     def get(self):
-        collection = self.collection
-        doc = collection.find_and_modify({"sort":{"_id":1}, "remove":True})
+        
+        doc = self.coll.find_and_modify(query={}, sort={"_id":1}, remove=True)
         return doc["item"]
-    
-    def add(self, item):
-        collection = self.collection
-        collection.insert({"item":item})
+
