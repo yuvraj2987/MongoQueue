339f44dbeae5d14c911e605078626e300bddb845
Amit Kulkarni <yuvraj2987@gmail.com>
2014-07-15 17:38:03 +0000

    Added logging statements

--- Mongo/queue.py
+++ Mongo/queue.py
@@ -1,29 +1,37 @@
-
+import logging
 
 
 class MongoQueue():
 
     def __init__(self, mongo_client, queue_name):
         self.client = mongo_client
+        self.name = queue_name
         self.db = mongo_client.messages
         self.coll = self.db[queue_name]
     
 
     def add(self, item):
+        logging.info("--- %s MongoQueue.add() ----", self.name)
         #self.client.write_concern={"w":1}
-        self.coll.insert({"item":item})
+        item_id = self.coll.insert({"item":item})
+        logging.debug("Insert item in MongoQueue with id=%s", item_id)
 
     def get(self):
         
         #self.client.write_concern = {"w":1}
+        logging.info("--- %s MongoQueue.get() ---", self.name)
         doc = self.coll.find_and_modify(query={}, sort={"_id":1}, remove=True)
         if doc is None:
+            logging.info(" %s MongoQueue is Empty", self.name)
             return None
         #else:
+        logging.debug("removing element with id=%s", doc["_id"])
         return doc["item"]
 
     def drop(self):
         """
         return True if this collection is dropped successfully
         """
+        logging.info("--- MongoQueue.drop() ---")
+        logging.info("%s Dropped", self.name)
         return self.db.drop_collection(self.coll)
